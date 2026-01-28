4bb8cbb5fe242f39f9152e248a0d3fc25c0baf0b
siamsamix <rsiamsami@gmail.com>
2026-01-28 12:13:45 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('labForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const physics = document.querySelector('input[name="physics"]:checked').value;
+    const chemistry = document.querySelector('input[name="chemistry"]:checked').value;
+    const programming = document.querySelector('input[name="programming"]:checked').value;
+    
+    // Here you would typically send the data to a server
+    // For demo purposes, we'll just show an alert
+    alert(`Your selections have been saved!\n\nPhysics: Group ${physics}\nChemistry: Group ${chemistry}\nProgramming: Group ${programming}`);
+    
+    // You could also redirect or show a success message
+    // window.location.href = 'success.html';
+});
